ab98b81cc987ca3ef4c9a8bf085d900cce603b39
jicool91 <jicool912@gmail.com>
2017-04-25 20:52:28 +0300

    add header

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="header">
+	<div class="header__logo">
+	
+	</div>
+	<div class="header__menu">
+		<ul class="header__menu-list">
+			<li class="header__menu-list__item"><a href="">Главная</a></li>
+			<li class="header__menu-list__item"><a href="">Каталог</a></li>
+			<li class="header__menu-list__item"><a href="">Контакты</a></li>
+			<li class="header__menu-list__item"><a href="">О нас</a></li>
+		</ul>
+	</div>
+</header>
