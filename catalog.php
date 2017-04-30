36f36f90222f2996674a3d22ea8b168e9cb8b4dd
jicool91 <jicool912@gmail.com>
2017-04-30 13:52:13 +0300

    zxc

--- catalog.php
+++ catalog.php
@@ -4,29 +4,114 @@
 <head>
 	<meta charset="UTF-8">
 	<title>NewShop</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
 	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 	<? include "header.php" ?>
+
+
 		<div class="goods">
 			<div class="container">
-
 				<div class="goods-box">
-					<h1 class="goods-box__heading">Какой-то цветок из бумаги</h1>
-					<div class="wrapper">
-						<img src="img/FullSizeRender-20-04-17-17-48.jpeg" alt="" class="goods-box__image">
-						<span class="goods-box__text"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum perferendis, adipisci nisi. Ipsum dolorem omnis itaque sunt delectus quam neque iste consequatur et eaque, perspiciatis quas tenetur tempore nisi praesentium?</span><span>Ullam enim eveniet amet recusandae vitae commodi, beatae repudiandae quidem officiis, aspernatur repellendus excepturi adipisci iusto id consequatur voluptatibus nemo? Sapiente, soluta. Labore similique deleniti aperiam aliquam consectetur sequi iure!</span><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aliquid, quod voluptas! Maxime, suscipit ex ullam aut minima obcaecati reprehenderit quasi error nesciunt recusandae placeat ad, magni sequi iusto alias?</span>
-						<span>Accusamus, autem, repellat animi, eum assumenda esse iure quos doloribus recusandae quis atque sed totam necessitatibus ipsa dolore! Iure deserunt iste quam minus reprehenderit deleniti dolorem, eos vitae. Eos, alias.</span>
-						<span>Soluta tempore illum delectus veritatis dignissimos, ducimus culpa ratione tempora esse voluptate recusandae error odit eos, aspernatur excepturi fugiat in incidunt. Ipsum id excepturi hic aliquam, cumque sequi, deleniti a!</span></span>
+					<h1 class="goods-box__heading">Топиарий. Ручная работа</h1>
+					<div class="goods-box__pictures">
+						<a href=""><img src="img/img1.jpg" alt="" class="goods-box__image"></a>
+						<a href=""><img src="img/img2.jpg" alt="" class="goods-box__image"></a>
+						<a href=""><img src="img/img3.jpg" alt="" class="goods-box__image"></a>
 					</div>
-					<label for="box-total" class="goods-box__price"><input type="text" id="box-total" class="goods-box__total-price"></label>
-				</div>
 
+
+
+					<table class="goods-box__table-description" border="1">
+					
+								<tr>
+									<th>Закажите готовые решения</th>
+									<th>Топиарий. Характеристики</th>
+									<th>Уникальный дизайн в 2 клика!</th>
+								</tr>
+								
+								
+						<tr class="row__description-buttons">
+							<td class="data__button-fix-price">
+
+								<div class="btn-group">
+									<button type="button" class="btn btn-success">Готовые решения за 960 &#8381 </button>
+									<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											<span class="caret"></span>
+											<span class="sr-only">Toggle Dropdown</span>
+										  </button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</div>
+
+
+
+							</td>
+							
+							<td class="data__table-description">
+								
+								
+								<div class="goods-box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum perferendis, adipisci nisi. Ipsum dolorem omnis itaque sunt delectus quam neque iste consequatur et eaque, perspiciatis quas tenetur tempore nisi praesentium?Ullam enim eveniet amet recusandae vitae commodi, beatae repudiandae quidem officiis, aspernatur repellendus excepturi adipisci iusto id consequatur voluptatibus nemo? Sapiente, soluta. Labore similique deleniti aperiam aliquam consectetur sequi iure!>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aliquid, quod voluptas!</div>
+
+
+								
+							
+
+
+							</td>
+							<td class="data__button-to-order">
+
+
+
+								<div class="btn-group">
+									<button type="button" class="button-to-order btn btn-danger">Уникальный дизайн</button>
+									<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											<span class="caret"></span>
+											<span class="sr-only">Toggle Dropdown</span>
+										  </button>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</div>
+
+								
+
+							</td>
+						</tr>
+						
+					
+						
+						
+					</table>
+					
+					
+
+
+
+
+
+
+
+
+
+
+				</div>
 			</div>
 		</div>
-
 
 
 </body>
